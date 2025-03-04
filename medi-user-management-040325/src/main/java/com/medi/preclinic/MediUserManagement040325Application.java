e644bd1747b1ab7120ebabb1a4fb34694255a1ac
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2025-03-04 11:56:54 +0530

    user-management-project

---
+++ medi-user-management-040325/src/main/java/com/medi/preclinic/MediUserManagement040325Application.java
@@ -0,0 +1,13 @@
+package com.medi.preclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediUserManagement040325Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediUserManagement040325Application.class, args);
+	}
+
+}
