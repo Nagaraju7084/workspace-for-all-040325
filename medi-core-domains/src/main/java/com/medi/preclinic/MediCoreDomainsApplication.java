d58a88c95d8a839094a72198fb856d76417fcffb
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2025-03-04 11:45:25 +0530

    core domains

---
+++ medi-core-domains/src/main/java/com/medi/preclinic/MediCoreDomainsApplication.java
@@ -0,0 +1,13 @@
+package com.medi.preclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediCoreDomainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediCoreDomainsApplication.class, args);
+	}
+
+}
