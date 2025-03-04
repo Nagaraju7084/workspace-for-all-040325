be81319285ae41674dc0f93f4f37540c7a8ffb46
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2025-03-04 19:50:31 +0530

    medi-user-management-040325 project

---
+++ medi-user-jwt-authentication-service-040325/src/main/java/com/medi/preclinic/MediUserJwtAuthenticationService040325Application.java
@@ -0,0 +1,13 @@
+package com.medi.preclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediUserJwtAuthenticationService040325Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediUserJwtAuthenticationService040325Application.class, args);
+	}
+
+}
---
+++ medi-user-jwt-authentication-service-040325/src/test/java/com/medi/preclinic/MediUserJwtAuthenticationService040325ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medi.preclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediUserJwtAuthenticationService040325ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
