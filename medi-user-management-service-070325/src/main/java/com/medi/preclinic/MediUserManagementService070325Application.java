ebf0021de6c56e1cbb3b07df0445dd7ee421bb9e
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2025-03-07 15:37:17 +0530

    medi-user-management-service-070325 created

---
+++ medi-user-management-service-070325/src/main/java/com/medi/preclinic/MediUserManagementService070325Application.java
@@ -0,0 +1,13 @@
+package com.medi.preclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediUserManagementService070325Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediUserManagementService070325Application.class, args);
+	}
+
+}
