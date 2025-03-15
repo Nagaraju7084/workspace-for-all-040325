62125f0862ce0277868692b81e7ba35668876d05
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2025-03-16 04:49:24 +0530

    building central authentication system / service which will speak to wso2 idp

---
+++ central-authentication-service-150325/src/main/java/com/medi/preclinic/CentralAuthenticationService150325Application.java
@@ -0,0 +1,13 @@
+package com.medi.preclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralAuthenticationService150325Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralAuthenticationService150325Application.class, args);
+	}
+
+}
