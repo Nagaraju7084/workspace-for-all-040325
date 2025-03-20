89d077da91c068d27cbdabb48f58b6ecfd7a808f
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2025-03-21 04:05:20 +0530

    getter and setters

--- medi-core-domains-60325/src/main/java/com/medi/preclinic/domain/Permission.java
+++ medi-core-domains-60325/src/main/java/com/medi/preclinic/domain/Permission.java
@@ -37,5 +37,41 @@ public class Permission implements Serializable {
 	
 	private String modifiedBy;
 	private Date modifiedDate;
-
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPermissionName() {
+		return permissionName;
+	}
+	public void setPermissionName(String permissionName) {
+		this.permissionName = permissionName;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	
 }
